8ae77691e85cf8b07f78bf6a5d4af65952410ab2
Prab Hiranayachatri <sys10m@Prabs-MacBook-Air-2.local>
2024-02-24 10:17:51 +0000

    create tables

--- server/app/tables.py
+++ server/app/tables.py
@@ -1,15 +1,40 @@
 from flask import current_app
 import models
 
-# TODO: Work on the structure of the tables
-def create_clients():
-    create_table_query = """
-            CREATE TABLE IF NOT EXISTS Clients (
-                email VARCHAR(255) PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                age INT,
-                
-            )
-            """
-    models.query_db(current_app.db_connection, create_table_query, ())
 
+def create_events():
+    create_event_table_query = """
+            CREATE TABLE IF NOT EXISTS Events (
+                id INT NOT NULL AUTO_INCREMENT,
+                name VARCHAR(255) NOT NULL,
+                max INT NOT NULL,
+                price INT NOT NULL,
+                date DATE,
+                PRIMARY KEY (id)
+            )
+    """
+    create_client_table_query = """
+            CREATE TABLE IF NOT EXISTS Clients (
+                id INT NOT NULL AUTO_INCREMENT
+                email VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                revTag VARCHAR(255),
+                eventID INT NOT NULL,
+                PRIMARY KEY (id)
+            )
+    """
+    create_ticket_table_query = """
+            CREATE TABLE IF NOT EXISTS Clients (
+                id INT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                revTag VARCHAR(255),
+                eventID INT NOT NULL,
+                amountDue INT NOT NULL,
+                amountPaid INT NOT NULL,
+                receiptID INT,
+                PRIMARY KEY (id)
+            )
+    """
+    models.query_db(current_app.db_connection, create_event_table_query, ())
+    models.query_db(current_app.db_connection, create_client_table_query, ())
+    models.query_db(current_app.db_connection, create_ticket_table_query, ())
