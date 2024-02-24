a5050eec01e0624cbb253935fb32da57a8ec5236
mykbit <96201443+mykbit@users.noreply.github.com>
2024-02-24 19:12:42 +0000

    Rework tables

--- server/app/tables.py
+++ server/app/tables.py
@@ -4,48 +4,37 @@ from .models import query_db, connect_db
 def create_events():
     create_event_table_query = """
             CREATE TABLE IF NOT EXISTS Events (
-                id INT NOT NULL AUTO_INCREMENT,
-                name VARCHAR(255) NOT NULL,
-                max INT NOT NULL,
-                price INT NOT NULL,
-                date DATE,
-                PRIMARY KEY (id)
+                id              INT             NOT NULL    AUTO_INCREMENT,
+                name            VARCHAR(255)    NOT NULL,
+                price           INT             NOT NULL,
+                date            DATE            NOT NULL,
+                holder_email    VARCHAR(255)    NOT NULL,
+                PRIMARY KEY (id),
+                FOREIGN KEY (holder_email) REFERENCES Clients(email)
             );
     """
+
     create_client_table_query = """
             CREATE TABLE IF NOT EXISTS Clients (
-                id INT NOT NULL AUTO_INCREMENT,
-                email VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL,
-                name VARCHAR(255) NOT NULL,
-                PRIMARY KEY (id)
+                email       VARCHAR(255)    NOT NULL,
+                password    VARCHAR(255)    NOT NULL,
+                name        VARCHAR(255)    NOT NULL,
+                PRIMARY KEY (email)
             );
     """
 
     create_ticket_table_query = """
             CREATE TABLE IF NOT EXISTS Tickets (
-                id INT NOT NULL,
-                name VARCHAR(255) NOT NULL,
-                revTag VARCHAR(255),
-                eventID INT NOT NULL,
-                amountDue INT NOT NULL,
-                amountPaid INT NOT NULL,
-                receiptID INT,
-                PRIMARY KEY (id)
+                id              INT             NOT NULL    AUTO_INCREMENT,
+                event_id        INT             NOT NULL,
+                client_email    VARCHAR(255)    NOT NULL,
+                valid           BOOLEAN         NOT NULL,
+                PRIMARY KEY (id),
+                FOREIGN KEY (event_id)      REFERENCES Events(id),
+                FOREIGN KEY (client_email)  REFERENCES Clients(email)
             );
     """
 
-    create_bankPayments_table_query = """
-            CREATE TABLE IF NOT EXISTS bankPayments (
-                id INT NOT NULL, 
-                timestamp TIMESTAMP NOT NULL, 
-                description VARCHAR(255) NOT NULL, 
-                amount INT,
-                PRIMARY KEY (id)
-            )
-
-    """
     query_db(connect_db(), create_event_table_query, ())
     query_db(connect_db(), create_client_table_query, ())
     query_db(connect_db(), create_ticket_table_query, ())
-    query_db(connect_db(), create_bankPayments_table_query, ())
